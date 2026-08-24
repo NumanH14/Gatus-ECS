b09c693abd8d9e6ff858bacc826c944b891eddcf
NumanH14 <numan.h1999@gmail.com>
2026-08-24 14:57:20 +0100

    feat: add ecr repo

---
+++ infra/modules/ecr/main.tf
@@ -0,0 +1,29 @@
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "gatus-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "example" {
+  repository = aws_ecr_repository.ecr_repo.id
+
+  policy = jsondecode({
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep last 10 images",
+      "selection": {
+        "tagStatus": "tagged",
+        "tagPrefixList": ["v"],
+        "countType": "imageCountMoreThan",
+        "countNumber": 10
+      },
+      "action": {
+        "type": "expire"
+      }
+    }]
+  })
+}
