9d5bf129f7b315f3cf0ff40c10bbae7d5e12a940
NumanH14 <numan.h1999@gmail.com>
2026-09-09 18:50:48 +0100

    refactor: ecs service

--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -21,6 +21,20 @@ resource "aws_cloudwatch_log_group" "cloudwatch_logging" {
   name = "cloudwatch-logging"
 }
 
+resource "aws_ecs_service" "gatus-service" {
+  name            = "gatus"
+  cluster         = aws_ecs_cluster.gatus-cluster.id
+  task_definition = aws_ecs_task_definition.gatus-ecs.id
+  desired_count   = 2
+  iam_role        = aws_iam_role.foo.arn
+  depends_on      = [aws_iam_role_policy.foo]
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alb-target-group.arn
+    container_name   = "gatus-image"
+    container_port   = 8080
+  }
+}
 resource "aws_ecs_task_definition" "gatus-ecs" {
   family                   = "gatus-task"
   requires_compatibilities = ["FARGATE"]
@@ -50,27 +64,3 @@ TASK_DEFINITION
   }
 }
 
-resource "aws_ecs_service" "gatus-service" {
-  name            = "gatus"
-  cluster         = aws_ecs_cluster.gatus-cluster.id
-  task_definition = aws_ecs_task_definition.gatus-ecs.id
-  desired_count   = 2
-  iam_role        = aws_iam_role.foo.arn
-  depends_on      = [aws_iam_role_policy.foo]
-
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.alb-target-group.arn
-    container_name   = "gatus"
-    container_port   = 8080
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
-}
