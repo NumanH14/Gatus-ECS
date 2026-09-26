22afa51b3fc7a563f943681afd4970494a5d48cf
NumanH14 <numan.h1999@gmail.com>
2026-09-26 15:18:31 +0100

    fix: point DNS record to apex domain

--- infra/modules/dns/main.tf
+++ infra/modules/dns/main.tf
@@ -13,14 +13,9 @@ provider "cloudflare" {
 
 resource "cloudflare_dns_record" "example_dns_record" {
   zone_id = var.cloudflare_api_token
-  name = "numanhussainprojects.com"
+  name = "@"
   ttl = 1
   type = "A"
   comment = "Domain verification record"
-  private_routing = true
-  proxied = true
-  settings = {
-    ipv4_only = true
-  }
   tags = ["owner:dns-team"]
 }
